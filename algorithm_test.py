4add7776679f0d025b43c53b21097c50eba60d77
Arepo <jinksy@gmail.com>
2021-09-15 20:08:31 +0100

    Complete modulo challenge

--- algorithm_test.py
+++ algorithm_test.py
@@ -1,4 +1,5 @@
 from .propagate_rightmost_set_bit import propagate_rightmost_set_bit as pr
+from .mod_power_of_two import modulo
 
 def test_it_propagates_the_rightmost_set_bit_in_big_O_of_1_time():
   bit_word = int('01010000', 2)
@@ -9,3 +10,83 @@ def test_it_computes_x_mod_a_power_of_two():
   assert modulo(77, 64) == 13
   assert modulo(13, 16) == 13
   assert modulo(0,  8)  == 0
+  assert modulo(10, 4)  == 2
+
+# original & (denom - 1)
+
+
+# 1001101 % # 77
+# 1000000 # 64
+# 0001101
+
+# 1001101 %
+# 0000100
+# 0000001
+
+# 100 %
+# 010
+# 000
+
+# 1000 %
+# 0010
+# 0000
+
+# 1000 %
+# 0100
+# 0000
+
+# 1111 %
+# 0100
+# 0011
+
+# 1111 %
+# 1000
+# 0111
+
+# 01111 %
+# 10000
+# 01111
+
+# 1101 %
+# 1000
+# 0101
+
+# 1001 %
+# 0100
+# 0001
+
+# 10111 % # 23
+# 00100
+# 00011
+
+
+
+# bin(int("0b1001101", 2) % int("0b1000000", 2))
+# int("0b1101", 2)
+
+# bin(int("0b100", 2) % int("0b010", 2))
+# int("0b0", 2)
+
+# bin(int("0b1000", 2) % int("0b0010", 2))
+# int("0b0", 2)
+
+# bin(int("0b1000", 2) %  int("0b0100", 2))
+# int("0b0", 2)
+
+# bin(int("0b1111", 2) % int("0b0100", 2))
+# int("0b11", 2)
+
+# bin(int("0b1111", 2) % int("0b1000", 2))
+# int("0b111", 2)
+
+# bin(int("0b01111", 2) % int("0b10000", 2))
+# int("0b1111", 2)
+
+# bin(int("0b1101", 2) % int("0b1000", 2))
+# int("0b101", 2)
+
+# bin(int("0b1001", 2) % int("0b0100", 2))
+# int("0b1", 2)
+
+# bin(int("0b10111", 2) % int("0b00100", 2))
+# int("0b11", 2)
---
+++ mod_power_of_two.py
@@ -0,0 +1,2 @@
+def modulo(numerator, denominator):
+  return numerator & (denominator - 1)
