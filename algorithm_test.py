84d7641c5e55157af3618fd13670e750091f0797
Arepo <jinksy@gmail.com>
2021-09-16 12:41:21 +0100

    Add tests for rectangle overlap

--- algorithm_test.py
+++ algorithm_test.py
@@ -1,23 +1,35 @@
 from .propagate_rightmost_set_bit import propagate_rightmost_set_bit as pr
 from .mod_power_of_two import modulo
 from .check_if_power_of_two import is_2_power
+from collections import namedtuple
 
-def test_it_propagates_the_rightmost_set_bit_in_big_O_of_1_time():
+# All in O(1) time, using bitwise operators, rquality checks, and Boolean operators
+
+def test_propagate_rightmost_set_bit():
   bit_word = int('01010000', 2)
   assert bin(pr(bit_word)) == '0b1011111'
 
-def test_it_computes_x_mod_a_power_of_two():
+def test_mod_power_of_two():
   # Assuming inputs are whole numbers
   assert modulo(77, 64) == 13
   assert modulo(13, 16) == 13
   assert modulo(0,  8)  == 0
   assert modulo(10, 4)  == 2
 
-def test_check_if_power_of_two():
+def check_if_power_of_two():
   assert is_2_power(0) == False
   assert is_2_power(1) == True
   assert is_2_power(2) == True
   assert is_2_power(3) == False
   assert is_2_power(16) == True
 
-
+def test_overlapping_rectangle():
+  Rectangle = namedtuple('Rect', ('x', 'y', 'width', 'height'))
+  rectangle_1 = Rectangle(0,0,4,3)
+  rectangle_2 = Rectangle(4,3,2,3)
+  rectangle_3 = Rectangle(2,2,3,5)
+  rectangle_4 = Rectangle(2,4,3,3)
+  assert overlapping_rectangle(rectangle_1, rectangle_2) == None
+  assert overlapping_rectangle(rectangle_1, rectangle_3) == Rectangle(2,2,2,1)
+  assert overlapping_rectangle(rectangle_3, rectangle_1) == Rectangle(2,2,2,1)
+  assert overlapping_rectangle(rectangle_4, rectangle_2) == Rectangle(4,4,2,1)
