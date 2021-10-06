e8e76a418ad2cd92bfd8a433e8cb20cf89075e61
Arepo <jinksy@gmail.com>
2021-10-06 10:20:57 +0100

    Solve matrix rotation

--- matrix_rotation.py
+++ matrix_rotation.py
@@ -1,13 +1,23 @@
 import pdb
 
-def rotate(matrix, i=0, j=0):
-  if j > len(matrix)/2:
+def rotate(matrix, index=0, layer=0):
+  if layer >= len(matrix)/2:
     return
 
-  first  = matrix[j][i]
-  second = matrix[i][-1 - j]
-  third  = matrix[-1 -j][-1 - i]
-  fourth = matrix[-1 - i][j]
-  pdb.set_trace()
+  first  = matrix[layer][index]
+  second = matrix[index][-1 - layer]
+  third  = matrix[-1 -layer][-1 - index]
+  fourth = matrix[-1 - index][layer]
+
+  matrix[layer][index]          = fourth
+  matrix[index][-1 - layer]     = first
+  matrix[-1 -layer][-1 - index] = second
+  matrix[-1 - index][layer]     = third
+
+  if index < len(matrix) - layer - 2:
+    rotate(matrix, index=index+1, layer=layer)
+  else:
+    layer += 1
+    rotate(matrix, index=layer, layer=layer)
 
   return
