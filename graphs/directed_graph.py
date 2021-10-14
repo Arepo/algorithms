13c77cb83fd33c22ab71bb687f28fde8280e9627
Arepo <jinksy@gmail.com>
2021-10-14 12:37:46 +0100

    Add test for pathfinding between directed nodes

---
+++ graphs/directed_graph.py
@@ -0,0 +1,5 @@
+class DGNode:
+  def __init__(self, data=None):
+    self.data = data
+    self.neighbours = []
+
--- graphs/graphtest.py
+++ graphs/graphtest.py
@@ -1,3 +1,4 @@
+import random
 from tree import BinaryTreeNode
 import pdb
 
@@ -21,4 +22,18 @@ def test_traversals():
   root.record_post_order_traversal(traversal_record)
   assert traversal_record == [3, 4, 1, 5, 2, 0]
 
+def test_route_finder():
+  origin = DGNode()
+  destination = DGNode()
 
+  # Build a set of spokes, including a one-way path from origin to destination
+  for i in range(0,5):
+    origin.neighbours.append(DGNode(i))
+  for i in range(5,15):
+    intermediary = random.choice(origin.neighbours)
+    intermediary.neighbours.append(DGNode)
+    if i == 14:
+      intermediary.neighbours[-1].append(destination)
+
+  assert origin.has_path_to(destination)
+  assert destination.has_path_to(origin) == False
