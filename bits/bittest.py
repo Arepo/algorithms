30976328bb96c34e2358fb2415d4008a320586cb
Arepo <jinksy@gmail.com>
2021-10-25 12:05:41 +0100

    Add test for max 1s

--- bits/bittest.py
+++ bits/bittest.py
@@ -1,9 +1,14 @@
 import pdb
 
-from real_numbers import binary_representation
+from bit_functions import binary_representation, max_1s
 
 def test_binary_representation():
   # Assume 0 <= input < 1
   assert binary_representation(0.5) == '0.1'
   assert binary_representation(0.125) == '0.001'
   assert binary_representation(0.79) == 'ERROR'
+
+def test_max_1s():
+  # Return the max consecutive 1s you could create with a single flip on the bit sequence of an integer
+  assert max_1s(9) == 2
+  assert max_1s(1775) == 8
