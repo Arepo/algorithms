aa80a845bd7491584324c89f5f73f8f26d5ffe0a
Arepo <jinksy@gmail.com>
2021-10-27 14:31:09 +0100

    Minor refactors

--- bits/bit_functions.py
+++ bits/bit_functions.py
@@ -1,7 +1,7 @@
 import math
 import pdb
 
-def binary_representation(num):
+def binary_representation(num) -> str:
   bitstring = ['0', '.']
   dyadic_fractions = set()
 
@@ -25,7 +25,7 @@ def binary_representation(num):
 
   return ''.join(bitstring)
 
-def max_1s(num):
+def max_1s(num) -> int:
   bitstring = f"{num:b}"
   str_start_index = 0
   substring_index = 0
@@ -60,7 +60,7 @@ def max_1s(num):
 
   return max_length
 
-def count_bit_transitions(num1, num2):
+def count_bit_transitions(num1, num2) -> int:
   exclusive_bits = num1 ^ num2
   transitions = 0
   while exclusive_bits != 0:
@@ -72,15 +72,17 @@ def draw_line(screen, screen_width, x1, x2, y):
   first_byte_index = _bytes_index(screen, screen_width, x1, y)
   last_byte_index = _bytes_index(screen, screen_width, x2, y)
 
-  if first_byte_index != last_byte_index:
+  if first_byte_index == last_byte_index:
+    screen[first_byte_index] = screen[first_byte_index] | (255 >> (_byte_index(x1) + 7 - _byte_index(x2))) << 7 - _byte_index(x2)
+  else:
     screen[first_byte_index] = screen[first_byte_index] | 255 & ~(255 << 8 - _byte_index(x1))
 
     for byte in range(first_byte_index + 1, last_byte_index):
+      # Intermediate bytes are all 1s
       screen[byte] = 255
 
     screen[last_byte_index] = screen[last_byte_index] | 255 & (255 << 8 - _byte_index(x2))
-  else:
-    screen[first_byte_index] = (255 >> (_byte_index(x1) + 7 - _byte_index(x2))) << 7 - _byte_index(x2)
+
 
 def _bytes_index(screen, width, x, y):
   height = int(len(screen) / width - 1)
